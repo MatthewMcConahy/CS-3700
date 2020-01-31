bc4ac7ab44d61bd7bc533aabd052728f62a13144
Matthew McConahy <matthewmcconahy123@gmail.com>
2020-01-30 23:31:54 -0500

    First Exam
    Enums, loops, ifs, using functions to develop a small RPG that is generated from set inputs.
    
    The class will need to be spread out. The code is currently all compiled to gether inside one class instead of being seperated

---
+++ MyClass/src/myclass/MyClass.java
@@ -0,0 +1,155 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myclass;
+
+
+
+/**
+ *
+ * @author matthewmcconahy
+ */
+public class MyClass extends Weapons{
+
+   // enums
+    LifeState healthly;
+    LifeState damaged;
+    LifeState critical;
+    
+    
+    static int userHealth = 100;
+    
+    
+    private void start(){
+        
+        // using interfaces 
+        MyClass test = new MyClass();
+        
+        test.type();
+        test.damage();
+        test.range();
+        test.lifeSpan();
+        test.swing();
+        test.shoot();
+        test.aliveOrDead();
+        
+        
+    }
+   
+    
+    public static void main(String[] args) {
+       new MyClass().start();
+    }
+    
+    public void checkHealth(){
+        
+    }
+
+    @Override
+    public void type() {
+        weaponType = "rope";
+        System.out.println("You have picked a " + weaponType);
+    }
+
+    @Override
+    public void damage() {
+        attack = 20;
+        System.out.println("You have done "+ attack + " damage" );
+    }
+
+    @Override
+    public void range() {
+        attackRange = 44;
+        System.out.println("Your weapon range is "+ attackRange);
+    }
+
+    @Override
+    public void lifeSpan() {
+        weaponLifeSpan = 10;
+        if(weaponLifeSpan < 20){
+            System.out.println("Your weapon is becoming weak");
+        }
+        else{
+            System.out.println("Your weapon is still strong");
+        }
+    }
+
+    @Override
+    public void swing() {
+        int strength = 3;
+        int fatigue = 1;
+        
+        for(strength = 3; strength >= fatigue; fatigue++){
+            System.out.println("You have swung and your strength is " + strength );
+            strength--;
+            
+        } 
+        if(strength < fatigue){
+            System.out.println("You are tired, must rest");
+        }
+    }
+
+    @Override
+    public void shoot() {
+        attack = 300;
+        System.out.println("You have been shot and have been hurt with "+ attack + "damage");
+        userHealth = userHealth - attack;
+        
+    }
+    
+    public int aliveOrDead(){
+        if(userHealth >= 100){
+            System.out.println("User is okay and has " + userHealth + " left");
+        }
+        else if(userHealth >= 50)
+        {
+            System.out.println("User is hurt and has " + userHealth + " left" );
+         }
+        else if(userHealth <= 0){
+            System.out.println("User is not alive ");
+        }
+        return userHealth;
+        
+    }
+}
+ enum LifeState {
+    healthy, damage, critical;
+   
+}
+
+
+ abstract class Weapons implements Gun, Sword {
+    
+    String weaponType = "hammer";
+    int attack = 4;
+    int attackRange = 150;
+    int weaponLifeSpan = 20;
+    
+    
+    public void type(){
+        System.out.println("You have picked a " + weaponType);
+    }
+    public void damage(){
+        System.out.println("you have done" + attack + "to enemy");
+    }
+    public void range(){
+        System.out.println("Your weapon does not have enough range in the " + attackRange + " area");
+    }
+    public void lifeSpan(){
+        System.out.println("Your weapon only has "+ weaponLifeSpan + "left");
+    }
+    
+}
+
+ interface Sword{
+    
+    public void swing();
+   
+}
+
+interface Gun {
+    
+    public void shoot();
+}
